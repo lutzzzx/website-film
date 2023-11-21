9b793a8014b32b552676201f1b589500e974b9e5
lutzzzx <lutzzzx@gmail.com>
2023-11-21 23:07:16 +0700

    koreksi kesalahan

--- pages/login-page.php
+++ pages/login-page.php
@@ -10,19 +10,21 @@
   <link href="https://fonts.googleapis.com/css2?family=Inter:wght@100;200;300;400;500;600;700;800;900&display=swap" rel="stylesheet">
 </head>
 <body>
-  <div class="container vh-100 d-flex align-items-center">
-    <div class="card bg-dark d-flex flex-column justify-content-between" style="width: 400px; height: 530px">
-      <div class="card-body">
-        <label for="nama">Nama</label><br>
-        <input type="text" name="" id="nama" class="input-form bg-dark mb-2">
-        <label for="email">E-mail</label>
-        <input type="email" name="" id="email" class="input-form bg-dark mb-2">
-      </div>
-      <div class="card-footer mt-auto text-center">
-        <input type="submit" value="Sign Up" class="btn btn-primary px-5 mb-3">
-        <p>Belum memiliki akun? <span><a href="register-page.php">Buat akun.</a></span> </p>
+  <form action="">
+    <div class="container vh-100 d-flex align-items-center">
+      <div class="card bg-dark d-flex flex-column justify-content-between" style="width: 400px; height: 530px">
+        <div class="card-body">
+          <label for="email">E-mail</label>
+          <input type="email" name="" id="email" class="input-form bg-dark mb-2">
+          <label for="password">Password</label>
+          <input type="password" name="" id="password" class="input-form bg-dark mb-2">
+        </div>
+        <div class="card-footer mt-auto text-center">
+          <input type="submit" value="Sign Up" class="btn btn-primary px-5 mb-3">
+          <p>Belum memiliki akun? <span><a href="register-page.php">Buat akun.</a></span> </p>
+        </div>
       </div>
     </div>
-  </div>
+  </form>
 </body>
 </html>
--- pages/register-page.php
+++ pages/register-page.php
@@ -10,21 +10,23 @@
   <link href="https://fonts.googleapis.com/css2?family=Inter:wght@100;200;300;400;500;600;700;800;900&display=swap" rel="stylesheet">
 </head>
 <body>
-  <div class="container vh-100 d-flex align-items-center">
-    <div class="card bg-dark d-flex flex-column justify-content-between" style="width: 400px; height: 530px">
-      <div class="card-body">
-        <label for="nama">Nama</label><br>
-        <input type="text" name="" id="nama" class="input-form bg-dark mb-2">
-        <label for="email">E-mail</label>
-        <input type="email" name="" id="email" class="input-form bg-dark mb-2">
-        <label for="password">Password</label>
-        <input type="password" name="" id="password" class="input-form bg-dark mb-2">
-      </div>
-      <div class="card-footer mt-auto text-center">
-        <input type="submit" value="Sign Up" class="btn btn-primary px-5 mb-3">
-        <p>Sudah memiliki akun? <span><a href="login-page.php">Masuk.</a></span> </p>
+  <form action="">
+    <div class="container vh-100 d-flex align-items-center">
+      <div class="card bg-dark d-flex flex-column justify-content-between" style="width: 400px; height: 530px">
+        <div class="card-body">
+          <label for="nama">Nama</label><br>
+          <input type="text" name="" id="nama" class="input-form bg-dark mb-2">
+          <label for="email">E-mail</label>
+          <input type="email" name="" id="email" class="input-form bg-dark mb-2">
+          <label for="password">Password</label>
+          <input type="password" name="" id="password" class="input-form bg-dark mb-2">
+        </div>
+        <div class="card-footer mt-auto text-center">
+          <input type="submit" value="Sign Up" class="btn btn-primary px-5 mb-3">
+          <p>Sudah memiliki akun? <span><a href="login-page.php">Masuk.</a></span> </p>
+        </div>
       </div>
     </div>
-  </div>
+  </form>
 </body>
 </html>
