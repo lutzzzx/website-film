23e4dfcbe7856b02294ee36787b968cfd30e14f0
iBalll <cordeno82@gmail.com>
2023-11-24 14:32:39 +0700

    penerapan while untuk menampilkan film pada index.php

--- index.php
+++ index.php
@@ -1,3 +1,16 @@
+<?php
+include('assets/php/database.php');
+
+$sql = "SELECT * FROM film";
+$query = mysqli_query($connect, $sql);
+
+$sqltop = "SELECT * FROM film ORDER BY jml_like DESC";
+$querytop = mysqli_query($connect, $sqltop);
+
+$id_user = $_SESSION['id_user'];
+
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -31,36 +44,19 @@
 
   <!-- SLIDER -->
   <div class="slider-container">
+  <?php while($top = mysqli_fetch_assoc($querytop)) { ?>
     <div class="slider">
-      <img class="slider-image" src="assets/images/background.jpg" alt="Foto 1">
+      <img class="slider-image" src="<?= $top['foto'] ?>" alt="Foto 1">
       <div class="slider-content">
         <div class="container">
           <h1 class="mb-3">TOP 5 MOVIES THIS WEEK</h1>
           <hr class="mb-3">
-          <h1 style="font-size: 48px">THE AVENGER</h1>
+          <h1 style="font-size: 48px"><?= $top['judul'] ?></h1>
         </div>
       </div>
     </div>
-    <div class="slider">
-      <img class="slider-image" src="assets/images/clapperboard.png" alt="Foto 2">
-      <div class="slider-content">
-        <div class="container">
-          <h1 class="mb-3">TOP 5 MOVIES THIS WEEK</h1>
-          <hr class="mb-3">
-          <h1 style="font-size: 48px">THE AVENGER 2</h1>
-        </div>
-      </div>
-    </div>
-    <div class="slider">
-      <img class="slider-image" src="assets/images/warning.png" alt="Foto 3">
-      <div class="slider-content">
-        <div class="container">
-          <h1 class="mb-3">TOP 5 MOVIES THIS WEEK</h1>
-          <hr class="mb-3">
-          <h1 style="font-size: 48px">THE AVENGER 3</h1>
-        </div>
-      </div>
-    </div>
+    <?php } ?>
+
     <img src="assets/images/previous.png" id="prev-button" onclick="prevSlide()"/>
     <img src="assets/images/next.png" id="next-button" onclick="nextSlide()"/>
   </div>
@@ -68,42 +64,28 @@
   <main class="container mt-5 mb-5">
     <h1 class="mb-4">ALL MOVIES</h1>
     <div class="d-flex flex-wrap gap-4">
-      <div class="card-movie">
-        <a href="" class="link-movie"> <!--Tambahkan link ke detail film-->
-          <img src="assets/images/background.jpg" alt="" class="img-wrapper mb-2">
-          <p class="text-center mb-2">Judul Film sdfsdf sdf saf sdfsdf s</p>
-        </a>
-        <div class="d-flex gap-3 justify-content-center">
-          <div class="bg-primary btn px-3 py-2 d-flex gap-2">
-            <img src="assets/images/like.png" alt="" width="16px">
-            <p style="font-size: 12px">123</p>
-          </div>
-          <div class="bg-primary btn px-3 py-2 d-flex gap-2">
-            <img src="assets/images/dislike.png" alt="" width="16px">
-            <p style="font-size: 12px">123</p>
-          </div>
-        </div>
-      </div>
-      <div class="card-movie">
-        <a href="" class="link-movie"> <!--Tambahkan link ke detail film-->
-          <img src="assets/images/background.jpg" alt="" class="img-wrapper mb-2">
-          <p class="text-center mb-2">Judul Film sdfsdf sdf saf sdfsdf s</p>
-        </a>
-        <div class="d-flex gap-3 justify-content-center">
-          <div class="bg-primary btn px-3 py-2 d-flex gap-2">
-            <img src="assets/images/like.png" alt="" width="16px">
-            <p style="font-size: 12px">123</p>
-          </div>
-          <div class="bg-primary btn px-3 py-2 d-flex gap-2">
-            <img src="assets/images/dislike.png" alt="" width="16px">
-            <p style="font-size: 12px">123</p>
-          </div>
-        </div>
-      </div>
+
+        <?php while($row = mysqli_fetch_assoc($query)){ ?>
+            <div class="card-movie">
+                <a href="" class="link-movie"> <!--Tambahkan link ke detail film-->
+                  <img src="<?= $row['foto'] ?>" alt="" class="img-wrapper mb-2">
+                  <p class="text-center mb-2"><?= $row['judul'] ?></p>
+                </a>
+                <div class="d-flex gap-3 justify-content-center">
+                  <div class="bg-primary btn px-3 py-2 d-flex gap-2">
+                    <img src="assets/images/like.png" alt="" width="16px">
+                    <p style="font-size: 12px"><?= $row['jml_like'] ?></p>
+                </div>
+                <div class="bg-primary btn px-3 py-2 d-flex gap-2">
+                    <img src="assets/images/dislike.png" alt="" width="16px">
+                    <p style="font-size: 12px"><?= $row['jml_dislike'] ?></p>
+                </div>
+              </div>
+            </div>
+        <?php } ?>
+
     </div>
   </main>
-
-  
 
   <script src="assets/js/slider.js"></script>
   
