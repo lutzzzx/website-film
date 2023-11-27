163f90f1326674f305914b3df849b1b30c29a0b6
lutzzzx <lutzzzx@gmail.com>
2023-11-27 13:44:06 +0700

    access-denied page

---
+++ admin/access-denied.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+  <link rel="stylesheet" href="../assets/css/style.css">
+</head>
+<body>
+  <div class="d-flex align-items-center justify-content-center" style="height: 100vh;">
+    <h1 class="">ANDA TIDAK MEMPUNYAI AKSES</h1>
+  </div>
+</body>
+</html>
